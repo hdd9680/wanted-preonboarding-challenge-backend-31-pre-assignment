743585a61ecd74562a9a5c8aa7615a41cc11bafa
hdd0680 <hdd0680@gmail.com>
2025-04-26 14:04:44 +0900

    스피링부트 프로젝트 이니셜

---
+++ src/main/java/com/hsy/wanted_preonboarding_challenge_backend_31_pre_assignment/WantedPreonboardingChallengeBackend31PreAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.hsy.wanted_preonboarding_challenge_backend_31_pre_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreonboardingChallengeBackend31PreAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedPreonboardingChallengeBackend31PreAssignmentApplication.class, args);
+	}
+
+}
